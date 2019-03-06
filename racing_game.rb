8cf6c0f39de14020fbcfbe14f816b8d4a76122ae
nikita <contactnikitakazakov@gmail.com>
2019-03-06 13:55:48 -0500

    Adding Game Class.

--- racing_game.rb
+++ racing_game.rb
@@ -42,7 +42,7 @@ class Car
   end
 
   def stats
-    puts "Car name: #{name}.  Color: #{color}.  Health: #{health}. Ignition: #{ignition}. Speed: #{speed}"
+    "Car name: #{name}.  Color: #{color}.  Health: #{health}. Ignition: #{ignition}. Speed: #{speed}"
   end
 
   def velocityIncrease
@@ -89,6 +89,78 @@ class Player
 
 end
 
+
+
+
+class Game
+
+  attr_reader :cars, :players, :title
+
+  def initialize(title)
+    @title = title
+    @players = []
+    @cars = []
+  end
+
+  def carsAdd(car)
+    @cars.push(car)
+  end
+
+  def carsRemove(car)
+    @cars.pop(car)
+  end
+
+  def playerAdd(player)
+    @players.push(player)
+  end
+
+  def playerRemove(player)
+    @player.pop(player)
+  end
+
+  def stats
+    puts "\n\n**********"
+    puts "Game Title:"
+    puts "\t#{title}"
+    puts "Cars:"
+
+    @cars.each do |car|
+      puts "\t#{car.stats}"
+    end
+
+    puts "Players:"
+    @players.each do |player|
+      puts "\t#{player.stats}"
+    end
+
+  end
+
+end
+
 player1 = Player.new("Jimmy")
 player2 = Player.new("Rico")
 player3 = Player.new("Bobby")
+
+
+car1 = Car.new("Ford")
+car2 = Car.new("Honda")
+car3 = Car.new("Toyota")
+car4 = Car.new("Renault")
+
+gameNFS = Game.new("NFS")
+
+
+gameNFS.carsAdd(car1)
+gameNFS.carsAdd(car2)
+gameNFS.carsAdd(car3)
+
+gameNFS.playerAdd(player1)
+gameNFS.playerAdd(player2)
+gameNFS.playerAdd(player3)
+
+
+gameNFS.stats
+
+
+
+#gameNFS.cars.each { |car| puts car.name }
